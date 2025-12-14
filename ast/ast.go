bc85ef8929f77cc4c87603e11570bcf86eb85af8
Claudio Brasser <cbrasser@protonmail.ch>
2025-12-14 13:08:51 +0100

    started working on the ast and the parser

---
+++ ast/ast.go
@@ -0,0 +1,54 @@
+/*
+Package ast
+*/
+package ast
+
+import (
+	"zebra/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program node will be the root node of every AST
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // The token.LET token)
+	Name  *Identifier // e.g. variable name
+	Value Expression  // The expression to be stored inthe variable
+}
+
+// Marker Method to declaer that a LetSTatement implements the statementNode interface
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral Actually implement the TokenLiteral Method required for StatementNodes
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // The token.IDENT TOken
+	Value string      // The variable name
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
